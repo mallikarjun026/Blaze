57c6d88e9f8ed324baaa59aeabc3eba3beadb048
mallikarjun026 <mallikarjunsh026@gmail.com>
2023-06-03 14:54:08 +0530

    updated .gitinnore pom and Browsermanager file

--- src/main/java/com/blaze/manager/BrowserManager.java
+++ src/main/java/com/blaze/manager/BrowserManager.java
@@ -2,6 +2,7 @@ package com.blaze.manager;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class BrowserManager {
 
@@ -26,9 +27,22 @@ public class BrowserManager {
     public synchronized void initializeWebdriver(String browserName){
         WebDriver driver=null;
         browserType=browserName;
-        if(this.browserType.equalsIgnoreCase("chrome"))
+        /*if(this.browserType.equalsIgnoreCase("chrome"))
         {
             driver= new ChromeDriver();
+
+        }*/
+        ChromeOptions options= new ChromeOptions();
+        switch(this.browserType.toLowerCase())
+        {
+            case "chrome":
+            options.addArguments("chrome.switches","--disable-extension");
+            options.addArguments("--disable-browser-side-navigation");
+            options.setAcceptInsecureCerts(true);
+            options.setExperimentalOption("useAutomationExtension",false);
+
+            driver= new ChromeDriver(options);
+            driver.manage().window().maximize();
 
         }
 
